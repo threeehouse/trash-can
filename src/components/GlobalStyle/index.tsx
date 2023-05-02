e4c8c2068786159f6b4a1c97e9ba651f98b4005b
dengoyoon <engus927@naver.com>
2023-05-03 01:35:56 +0900

    Global Style 만들고 적용

---
+++ src/components/GlobalStyle/index.tsx
@@ -0,0 +1,62 @@
+import { Global, css } from '@emotion/react';
+
+export function GlobalStyle() {
+  return (
+    <>
+      <link href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css" rel="stylesheet" type="text/css" />
+      <Global styles={globalCss} />
+    </>
+  );
+}
+
+const globalCss = css`
+  body {
+    margin: 0;
+    overflow-x: hidden;
+    height: 100vh;
+    color: #ffffff;
+
+    font-family: 'Spoqa Han Sans Neo', 'sans-serif';
+    font-size: 12px;
+    box-sizing: border-box;
+    word-break: keep-all;
+    overflow-wrap: break-word;
+
+    background-color: #242424;
+  }
+
+  button {
+    border: none;
+    background: transparent;
+    cursor: pointer;
+    padding: 0;
+    margin: 0;
+  }
+
+  input,
+  select,
+  textarea,
+  button {
+    font-family: inherit;
+    color: inherit;
+  }
+
+  a,
+  a:focus,
+  a:hover {
+    text-decoration: none;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0;
+  }
+
+  ul {
+    margin: 0;
+  }
+`;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import type { AppProps } from 'next/app';
 
-import { Navigation } from '../components';
+import { GlobalStyle, Navigation } from '../components';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <GlobalStyle />
       <Component {...pageProps} />
       <Navigation />
     </div>
