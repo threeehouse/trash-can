fa7456b5b107dda86e73d8aa3e1615a78a53e59b
dengoyoon <engus927@naver.com>
2023-05-03 03:45:51 +0900

    네비게이션 디자인 완료

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,10 +1,26 @@
+import styled from '@emotion/styled';
+
+import { theme } from '../../theme';
 import { Button } from '../Button';
 
 export function Navigation() {
   return (
-    <div>
-      <Button type="primary">Home</Button>
-      <Button type="ghost">Rank</Button>
-    </div>
+    <StyledNav>
+      <Button type="primary">HOME</Button>
+      <Button type="ghost">RANK</Button>
+    </StyledNav>
   );
 }
+
+const StyledNav = styled('nav')`
+  background-color: ${theme.colors.gray100};
+  border-radius: 32px;
+  padding: 6px;
+  z-index: 1;
+  position: fixed;
+  bottom: 12px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  width: 400px;
+`;
