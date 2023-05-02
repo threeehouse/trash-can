71b3be5cfdee4ebffe7ac5af415df609e50b4e9b
DuHyeon Yoon <engus927@naver.com>
2023-05-03 01:48:25 +0900

    네비게이션 일단 생성하고 글로벌 디자인, theme 생성 (#3)
    * 네비게이션 컴포넌트 일단 만듬
    
    * emotion 설치
    
    * Global Style 만들고 적용
    
    * 똑스 컬러토큰 일단 가져옴
    
    * theme color로 배경화면 변경

---
+++ src/components/GlobalStyle/index.tsx
@@ -0,0 +1,64 @@
+import { Global, css } from '@emotion/react';
+
+import { theme } from '../../theme';
+
+export function GlobalStyle() {
+  return (
+    <>
+      <link href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css" rel="stylesheet" type="text/css" />
+      <Global styles={globalCss} />
+    </>
+  );
+}
+
+const globalCss = css`
+  body {
+    margin: 0;
+    overflow-x: hidden;
+    height: 100vh;
+    color: #ffffff;
+
+    font-family: 'Spoqa Han Sans Neo', 'sans-serif';
+    font-size: 12px;
+    box-sizing: border-box;
+    word-break: keep-all;
+    overflow-wrap: break-word;
+
+    background-color: ${theme.colors.gray120};
+  }
+
+  button {
+    border: none;
+    background: transparent;
+    cursor: pointer;
+    padding: 0;
+    margin: 0;
+  }
+
+  input,
+  select,
+  textarea,
+  button {
+    font-family: inherit;
+    color: inherit;
+  }
+
+  a,
+  a:focus,
+  a:hover {
+    text-decoration: none;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0;
+  }
+
+  ul {
+    margin: 0;
+  }
+`;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,13 @@
 import type { AppProps } from 'next/app';
 
+import { GlobalStyle, Navigation } from '../components';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <GlobalStyle />
+      <Component {...pageProps} />
+      <Navigation />
+    </div>
+  );
 }
