fd789ee429533af4e1394f2828dd4aa833ede3e9
dengoyoon <engus927@naver.com>
2023-05-04 23:20:14 +0900

    페이지 전환 효과 구현, 하지만 제대로 안됨

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
+import { AnimatePresence } from 'framer-motion';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
 import { GlobalStyle, Navigation } from '../components';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return (
     <div>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <AnimatePresence mode="popLayout" initial={false}>
+        <Component {...pageProps} key={router.route} />
+      </AnimatePresence>
       <Navigation />
     </div>
   );
