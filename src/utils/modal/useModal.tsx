8501bb63dbf74f9339f789593246287c8ddfa497
dengoyoon <engus927@naver.com>
2023-05-16 17:58:17 +0900

    useModal을 만들다가 중단함

---
+++ src/utils/modal/useModal.tsx
@@ -0,0 +1,8 @@
+// import { useOverlay } from '../overlay';
+
+export const useModal = () => {
+  //   const overlay = useOverlay();
+  //   const openModal = () =>
+  //     new Promise(resolve =>
+  //       overlay.open(({ isOpen })))
+};
