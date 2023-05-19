9db2be5f1ad05f354a0a776249fe47b9b01d1a1c
dengoyoon <engus927@naver.com>
2023-05-19 19:42:57 +0900

    API call 테스트 성공

---
+++ src/pages/api/rank/index.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  return res.status(200).json({
+    isSuccess: true,
+    code: 2000,
+    message: 'hello world',
+  });
+}
