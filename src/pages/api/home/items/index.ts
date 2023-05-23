9fbca65eae0c6a124fa225cd837d97078622537e
dengoyoon <engus927@naver.com>
2023-05-23 19:25:45 +0900

    조회 api구현했으나 connection문제 발견

---
+++ src/pages/api/home/items/index.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import connectDB from '../../connectDB';
+import { ItemModel } from '../../model';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  connectDB();
+  const { method } = req;
+  if (method !== 'GET') {
+    return res.status(405).json({
+      isSuccess: false,
+      message: '허용되지 않은 메서드 입니다.',
+    });
+  }
+
+  try {
+    const items = await ItemModel.find();
+    return res.status(200).json({
+      isSuccess: true,
+      message: '아이템 조회에 성공하였습니다.',
+      result: items,
+    });
+  } catch {
+    return res.status(400).json({
+      isSuccess: false,
+      message: '아이템 조회에 실패하였습니다.',
+    });
+  }
+}
